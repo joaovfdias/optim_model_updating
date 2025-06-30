a3d1b127a076cea790575efd6e7701767f67c399
Joao Victor Dias <joao.v.dias@ufes.br>
2025-06-30 15:57:08 -0300

    Added compare individuals to individual Added compare individuals to individual.

--- optimization/individual.py
+++ optimization/individual.py
@@ -9,6 +9,14 @@ class Individual:
     def evaluate(self):
         self.fitness, self.data = self.fitness_function(self.param)
 
+    @staticmethod
+    def compareIndividuals(ind1, ind2):
+        for i, _ in enumerate(ind1.param):
+            if ind1.param[i] != ind2.param[i]:
+                return False
+        return True
+
+
     def __str__(self):
         param_str = ", ".join(f"{p:.4f}" for p in self.param)
         return f"Individual(param=[{param_str}], fitness={self.fitness:.4f})"
