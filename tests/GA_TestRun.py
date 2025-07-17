49c7998d30ac23f98e9e425166a070dad325637e
thiagoarturs <thiago.arthursouza@gmail.com>
2025-07-17 11:47:46 -0300

    Fixed "create_log" behavior when log_title is None. Added Ackley benchmark function to TestRuns.

--- tests/GA_TestRun.py
+++ tests/GA_TestRun.py
@@ -1,25 +1,40 @@
-from optimization.parameter import Continuous
+from optimization.parameter import *
 from optimization.ga_optimizer import GA
 
+import numpy as np
 
-def fitness_function(param):
-    x, y, z = param
-    return abs(x ** 3 + y ** 3 + z ** 3 - 42)
 
-# arquivo log
-log = "full"
+def ackley(params):
+    a = 20
+    b = 0.2
+    c = 2 * np.pi
+    params = np.array(params)
+    n = len(params)
+    sum_sq = np.sum(params ** 2)
+    sum_cos = np.sum(np.cos(c * params))
+    term1 = -a * np.exp(-b * np.sqrt(sum_sq / n))
+    term2 = -np.exp(sum_cos / n)
+    return term1 + term2 + a + np.exp(1)
 
-# parâmetros do algoritmo
-parameters = [Continuous(-8,8, 'x'), Continuous(-8, 12, 'y'), Continuous(-25, 25, 'z')]
+# parâmetros do modelo:
+parameters = [Continuous(-32.768,32.768,f"v{i}") for i in range(5)]
 
+# parâmetros do algoritmo:
 elitism_rate = 0.10
-crossover_rate = 0.60
-mutation_strength = 0.10 # no tipo de mutação "gaussian" para variáveis contínuas, alterar essa taxa diretamente (define a faixa percentual em que os paramêtros podem variar)
+crossover_rate = 0.40
+mutation_strength = 0.30
 
-population_size = 10
-generations = 10
+population_size = 100
+generations = 200
 
-# chamada
-rodada = GA(fitness_function, parameters, population_size, elitism_rate, crossover_rate, mutation_strength)
-#rodada.set_log(log_name, log_dir)
+# declaração do otimizador:
+rodada = GA(ackley, parameters, population_size, elitism_rate, crossover_rate, mutation_strength)
+
+# ajuste do registro:
+log = "full" # tipo de registro (True: simplificado, "full": todos os indivíduos)
+log_title = "GA_ackley" # alterar nome do arquivo gerado, se quiser
+log_dir = None # alterar diretório do registro, por padrão \log (lembre-se de usar o formato r"{caminho}" para declarar diretórios)
+rodada.set_log(log_title, log_dir)
+
+# chamada:
 best = rodada.run(generations, log=log)
--- tests/PSO_TestRun.py
+++ tests/PSO_TestRun.py
@@ -1,18 +1,23 @@
 from optimization.parameter import Continuous
 from optimization.pso_optimizer.pso_optimizer import PSO
 
+import numpy as np
 
-# declaração da função objetivo:
-def fitness_function(param):
-    x, y, z = param
-    return abs(x**3 + y**3 + z**3 - 42)
+
+def ackley(params):
+    a = 20
+    b = 0.2
+    c = 2 * np.pi
+    params = np.array(params)
+    n = len(params)
+    sum_sq = np.sum(params ** 2)
+    sum_cos = np.sum(np.cos(c * params))
+    term1 = -a * np.exp(-b * np.sqrt(sum_sq / n))
+    term2 = -np.exp(sum_cos / n)
+    return term1 + term2 + a + np.exp(1)
 
 # parâmetros do modelo:
-parameters =    [
-                Continuous(-8,8, 'x'),
-                Continuous(-8, 12, 'y'),
-                Continuous(-25, 25, 'z')
-                ]
+parameters = [Continuous(-32.768,32.768,f"v{i}") for i in range(5)]
 
 # parâmetros do algoritmo:
 w = 0.6 # inércia
@@ -21,15 +26,15 @@ c1 = 2.05 # governa a exploração da população
 c2 = 2.05 # governa a convergência
 init_vel_ratio = 0.2 # proporção do espaço de busca que pode ser empregado para velocidade inicial
 
-population_size = 100
+population_size = 50
 iteracoes = 100
 
 # declarção do otimizador:
-rodada = PSO(fitness_function, parameters, population_size, w, w_rate, c1, c2, init_vel_ratio)
+rodada = PSO(ackley, parameters, population_size, w, w_rate, c1, c2, init_vel_ratio)
 
 # ajuste do registro:
 log = "full" # tipo de registro (True: simplificado, "full": todos os indivíduos)
-log_title = None # alterar nome do arquivo gerado, se quiser
+log_title = "PSO_ackley" # alterar nome do arquivo gerado, se quiser
 log_dir = None # alterar diretório do registro, por padrão \log (lembre-se de usar o formato r"{caminho}" para declarar diretórios)
 rodada.set_log(log_title, log_dir)
 
