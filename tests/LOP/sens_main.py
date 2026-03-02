69b869579c59df56ca279513e352593a1b01e11d
Thiago LEST2 <thiago.arthursouza@gmail.com>
2026-03-02 11:40:52 -0300

    sens_main.py para LOP.

---
+++ tests/LOP/sens_main.py
@@ -0,0 +1,84 @@
+from optimization.sensitivity import SensitivityAnalyzer, ParamSpec, Sampler
+import pandas as pd
+import numpy as np
+from optimization.parameter import *
+from external.parser import Ansys
+from external.special_functions import SpecialFun
+
+import time
+import os
+
+
+def sensitivity_analysis(parameters, base_dir, ansys_exe_path=None):
+    keys = [p.key for p in parameters]
+
+    ansys_exe_path = ansys_exe_path or r"C:\Program Files\ANSYS Inc\ANSYS Student\v252\commonfiles\launcherQT\src\..\..\..\ansys\bin\winx64\MAPDL.EXE"
+
+    # base_dir = r"C:\Users\Thiago Artur\OneDrive\Documentos\2025.2\Problema 3\Py\Input\Análise 8"
+    ansys_working_dir = os.path.join(base_dir, 'ANSYS')
+    input_dir = os.path.join(base_dir, 'input')
+    output_dir = os.path.join(os.getcwd(), 'output')
+
+    base_script_filename = "scriptLOP.mac"
+    base_freq_filename = "out_freq.txt"
+    base_modes_filename = "out_modos_y.txt"
+
+    out_freq_filename = "out_freq.txt"
+    out_modes_filename = "out_modes.txt"
+
+    ansys = Ansys(ansys_exe_path, ansys_working_dir, input_dir, base_script_filename, base_freq_filename, base_modes_filename, output_dir)
+    ansys.set_output_filenames(out_freq_filename, out_modes_filename)
+    ansys.max_attempts = 6
+
+    def evaluate(row):
+        param = row
+
+        input_file = ansys.create_input_file(param, keys)
+        ansys.run_ansys(input_file, True, True)
+
+        comp_freq  = ansys.read_frequencies()
+        comp_modes = ansys.read_modes()
+
+        paired_comp_freq, paired_comp_modes, mac_error_sum = SpecialFun.pair_modes_mac(
+            comp_freq, comp_modes, ansys.base_modes
+        )
+        freq_error_sum = SpecialFun.norm_freq_errors(ansys.base_freq, paired_comp_freq)
+
+        peso_freq = 1
+        peso_mac  = 1
+        fitness = peso_freq * freq_error_sum + peso_mac * mac_error_sum
+
+        ddata = {}
+        for i, f in enumerate(paired_comp_freq, start=1):
+            ddata[f"freq #{i}"] = f
+        for i, m in enumerate(paired_comp_modes, start=1):
+            mac = SpecialFun.modal_assurance_criterion(comp_modes[i-1], m)
+            ddata[f"MAC #{i}"] = mac
+
+        return ddata
+
+    start_time = time.time()
+
+    # Gera DoE sem precisar do Optimizer
+    rng = np.random.default_rng()
+    specs = [ParamSpec(p.key, p.lower_bound, p.upper_bound) for p in parameters]
+
+    amostragem = 100*len(parameters)
+
+    X = Sampler.lhs(amostragem, specs, rng)
+    Y = X.apply(evaluate, axis=1, result_type="expand")
+
+    df_eval = pd.concat([X, Y], axis=1)
+
+    # Rodar análise de sensibilidade
+    sa = SensitivityAnalyzer(minimize=True)
+    selected = sa.workflow(df_eval, fitness_col="Fitness", interactive=False)
+    print("Parâmetros escolhidos:", selected)
+
+    end_time = time.time()
+    elapsed = end_time - start_time
+
+    print(f"\nTempo para amostragem de {amostragem} indivíduos ({amostragem/len(parameters)}x o número de parâmetros: "
+          f"\n{elapsed:.4f} s")
+
+    return
