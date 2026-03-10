4c333176456735cc0697bc4d3ff852fb19ade615
Thiago LEST2 <thiago.arthursouza@gmail.com>
2026-03-10 05:44:57 -0300

    prob. 2

--- tests/indexador_2026.py
+++ tests/indexador_2026.py
@@ -28,11 +28,11 @@ def indexar_problema(problema, full=False):
         script_name = 'scriptVIGA.mac'
         noise = None
         parameters = [
-            Continuous(20e9, 30e9, 'modulo'),
-            Continuous(0.1, 0.49, 'poisson'),
+            Continuous(20e9, 25e9, 'modulo'),
+            Continuous(0.1, 0.40, 'poisson'),
             Continuous(2400, 2600, 'dens'),
-            Continuous(10e6, 10e8, 'rigidez1'),
-            Continuous(10e6, 10e8, 'rigidez2')
+            Continuous(10e6, 10e7, 'rigidez1'),
+            Continuous(10e6, 10e7, 'rigidez2')
         ]
         target_params = [23e9, 0.2, 2500, 1e7, 1.5e7]
         key_to_name = {
