c307416c1ccd6b9219c87e100ce595a80426acd3
Thiago LEST <thiago.arthursouza@gmail.com>
2026-03-18 14:44:26 -0300

    [hold] adição de variáveis inteiras.

--- optimization/parameter.py
+++ optimization/parameter.py
@@ -24,6 +24,24 @@ class Continuous(Parameter):
         """
         return self.lower_bound <= value <= self.upper_bound
 
+class Integer(Parameter):
+    def __init__ (self, lower_bound: int, upper_bound: int, key: list[str]):
+        super().__init__(key)
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+        self.search_space = upper_bound - lower_bound
+
+    def random_value(self):
+        return random.randint(self.lower_bound,self.upper_bound)
+
+    def check_bounds(self, value):
+        """
+        Verifica se um valor está dentro dos limites do parâmetro.
+        :param value: Valor a ser verificado.
+        :return: True se o valor está dentro dos limites, False caso contrário.
+        """
+        return self.lower_bound <= value <= self.upper_bound
+
 class Binary(Parameter):
     def __init__ (self, key):
         super().__init__(key)
